349116136fcd8386d8b866c6f72a8c13ba1967bd
芙鲁米格 <EL-PSY-CONGRO@OUTLOOK.COM>
2019-07-06 08:25:51 +0800

    update

--- KV58 For mentally retarded car  Main Core/settings/demoLQ_KV58F24.Release.cspy.ps1
+++ KV58 For mentally retarded car  Main Core/settings/demoLQ_KV58F24.Release.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\Project\Mentally retarded car\KV58 For mentally retarded car\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.general.xcl" --backend -f "D:\Project\Mentally retarded car\KV58 For mentally retarded car\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.driver.xcl" 
+& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\Project\Github Project\RobotAssociation\KV58-For-Mentally-Retarded-Car-Version-Dual-Core\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.general.xcl" --backend -f "D:\Project\Github Project\RobotAssociation\KV58-For-Mentally-Retarded-Car-Version-Dual-Core\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.driver.xcl" 
 }
 else
 {
-& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\Project\Mentally retarded car\KV58 For mentally retarded car\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.general.xcl" --debug_file=$debugfile --backend -f "D:\Project\Mentally retarded car\KV58 For mentally retarded car\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.driver.xcl" 
+& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\Project\Github Project\RobotAssociation\KV58-For-Mentally-Retarded-Car-Version-Dual-Core\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.general.xcl" --debug_file=$debugfile --backend -f "D:\Project\Github Project\RobotAssociation\KV58-For-Mentally-Retarded-Car-Version-Dual-Core\KV58 For mentally retarded car  Main Core\settings\demoLQ_KV58F24.Release.driver.xcl" 
 }
